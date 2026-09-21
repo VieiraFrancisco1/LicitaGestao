3c47a41c540d3acb398f2ea531e0e5fc09ea6a28
Francisco Vieira <0vieira.francisco0@gmail.com>
2026-09-21 11:25:49 -0300

    Carrega correção final do login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './tenders-compact.css';
+import './login-right-fix.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
